d9103f7feb02bee67767927ee731625f48e2ab22
Rahul Rajendran <Rahul.Rajendran@gds.ey.com>
2022-11-25 18:02:37 +0530

    compare files

--- index.js
+++ index.js
@@ -13,20 +13,43 @@ try {
 
   // Split using a space character
   let files = filesList.split(' ');
+  const baseRepoDir = 'stage-branch';
 
   for (let file of files) {
     console.log(`file from loop js ${file}!`);
-    let content = fs.readFileSync(file, 'utf8')
+    let prFileContent = fs.readFileSync(file, 'utf8')
     console.log(`file from loop js ${content}!`);
+
+    console.log(`file from baseRepoDir loop js ${file}!`);
+    let baseFileContent = fs.readFileSync(path.join(baseRepoDir,file), 'utf8')
+    console.log(`file from baseRepoDir loop js ${content}!`);
+
+    const baseJson = JSON.parse(baseFileContent);
+    //console.log(baseJson[0]);
+  
+    const prJson = JSON.parse(prFileContent);
+    //console.log(prBaseJson[0]);
+  
+    const reposTobuild = [];
+  
+    for(prProduct of prJson){
+      const baseProduct = baseJson.find((baseProduct)=>baseProduct.name == prProduct.name);
+      if(prProduct.version != baseProduct.version){
+          reposTobuild.push(prProduct)
+      };
+    }
+  
+    console.log(reposTobuild);
+
   }
 
-  const baseRepoDir = 'stage-branch';
+  core.setOutput("repos", JSON.stringify(reposTobuild));
   
-  for (let file of files) {
-    console.log(`file from baseRepoDir loop js ${file}!`);
-    let content = fs.readFileSync(path.join(baseRepoDir,file), 'utf8')
-    console.log(`file from baseRepoDir loop js ${content}!`);
-  }
+  // for (let file of files) {
+  //   console.log(`file from baseRepoDir loop js ${file}!`);
+  //   let content = fs.readFileSync(path.join(baseRepoDir,file), 'utf8')
+  //   console.log(`file from baseRepoDir loop js ${content}!`);
+  // }
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
